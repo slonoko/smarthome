e38bff6df6b3038d3e173469273e97516a9d83d3
Elie Khoury <elie.kh@gmail.com>
2019-12-17 14:59:57 +0000

    fixed queries

--- backend/services/temperature/views.py
+++ backend/services/temperature/views.py
@@ -1,9 +1,20 @@
 from flask import json, request, Flask, Blueprint, current_app
 import smarthome_utils as h
 from services.temperature.models import Temperature
+import datetime
 
 temperature = Blueprint('temperature', __name__, url_prefix='/temperature')
 
+'''
+if i want to use millies
+from ms  to datetime --> datetime.datetime.fromtimestamp(ms/1000.0)
+from datetime to ms --> dt_obj.timestamp() * 1000
+
+url example: http://10.0.0.6:5000/temperature/?from_date=17.12.2019-13:00&to_date=17.12.2019-17:00
+'''
+
+def toDate(dateString): 
+    return datetime.datetime.strptime(dateString, "%d.%m.%Y-%H:%M").date()
 
 @temperature.route('/')
 def current():
@@ -13,5 +24,7 @@ def current():
 
 @temperature.route("/range")
 def range():
-    from_date = request.args.get('from_date')
-    to_date = request.args.get('to_date')
+    from_date = request.args.get('from_date', default = datetime.date.today(), type = toDate)
+    to_date = request.args.get('to_date', default = datetime.date.today(), type = toDate)
+    list_temps = Temperature.query.filter_by(Temperature.timestamp >= from_date and Temperature.timestamp <= to_date).order_by(Temperature.timestamp.desc())
+    return h.getResponse(current_app,list_temps[0].to_json())
