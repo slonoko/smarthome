5193473619d8b0e822705ed6873a8618818d9ce5
Elie Khoury <elie.kh@gmail.com>
2019-12-17 15:42:39 +0000

    between range

--- backend/services/temperature/views.py
+++ backend/services/temperature/views.py
@@ -2,7 +2,7 @@ from flask import json, request, Flask, Blueprint, current_app
 import smarthome_utils as h
 from services.temperature.models import Temperature
 import datetime
-
+from sqlalchemy import *
 temperature = Blueprint('temperature', __name__, url_prefix='/temperature')
 
 '''
@@ -10,7 +10,7 @@ if i want to use millies
 from ms  to datetime --> datetime.datetime.fromtimestamp(ms/1000.0)
 from datetime to ms --> dt_obj.timestamp() * 1000
 
-url example: http://10.0.0.6:5000/temperature/?from_date=17.12.2019-13:00&to_date=17.12.2019-17:00
+url example: http://10.0.0.6:5000/temperature/range?from_date=17.12.2019-12:00&to_date=18.12.2019-17:00
 '''
 
 def toDate(dateString): 
@@ -26,5 +26,7 @@ def current():
 def range():
     from_date = request.args.get('from_date', default = datetime.date.today(), type = toDate)
     to_date = request.args.get('to_date', default = datetime.date.today(), type = toDate)
-    list_temps = Temperature.query.filter_by(Temperature.timestamp >= from_date and Temperature.timestamp <= to_date).order_by(Temperature.timestamp.desc())
+    print(Temperature.query.filter(Temperature.timestamp.between(from_date,  to_date)).order_by(Temperature.timestamp.desc()))
+    list_temps = Temperature.query.filter(Temperature.timestamp.between(from_date,  to_date)).order_by(Temperature.timestamp.desc()).all()
+    print(list_temps)
     return h.getResponse(current_app,list_temps[0].to_json())
