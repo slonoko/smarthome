8018e3ae296f5417995bcdf9f5221f892fe6ce67
Elie Khoury <elie.kh@gmail.com>
2019-12-17 15:45:12 +0000

    dates

--- backend/services/dust/views.py
+++ backend/services/dust/views.py
@@ -1,9 +1,12 @@
 from flask import json, request, Flask, Blueprint, current_app
 from services.dust.models import Dust
 import smarthome_utils as h
+import datetime
 
 dust = Blueprint('dust', __name__, url_prefix='/dust')
 
+def toDate(dateString): 
+    return datetime.datetime.strptime(dateString, "%d.%m.%Y-%H:%M").date()
 
 @dust.route('/')
 def current():
@@ -13,5 +16,9 @@ def current():
 
 @dust.route("/range")
 def range():
-    from_date = request.args.get('from_date')
-    to_date = request.args.get('to_date')
+    from_date = request.args.get('from_date', default = datetime.date.today(), type = toDate)
+    to_date = request.args.get('to_date', default = datetime.date.today(), type = toDate)
+    print(Dust.query.filter(Dust.startdate.between(from_date,  to_date)).order_by(Dust.startdate.desc()))
+    list_dusts = Dust.query.filter(Dust.startdate.between(from_date,  to_date)).order_by(Dust.startdate.desc()).all()
+    print(list_dusts)
+    return h.getResponse(current_app,list_dusts[0].to_json())
