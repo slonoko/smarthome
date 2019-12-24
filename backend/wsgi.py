7aed0d6613b1327063af9952cac00b4f3f96fa2c
Elie Khoury <elie@el-khoury.net>
2019-12-24 20:47:59 +0100

    preparing docker for backend

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,4 +2,4 @@ from services import app
 
 
 if __name__ == '__main__': 
-    app.run('0.0.0.0')
+    app.run()
--- utils/smarthome_utils.py
+++ utils/smarthome_utils.py
@@ -1,11 +1,16 @@
 import argparse, json, sys
 import datetime
+import os
 
 def read_configuration():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config",  nargs='+', help="Configuration file in json format")
-    args = parser.parse_args()
-    json_file = args.config[0]
+    if "app_config" in os.environ:
+        json_file = os.getenv("app_config")
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--config",  nargs='+', help="Configuration file in json format")
+        args = parser.parse_args()
+        json_file = args.config[0]
+
     config = {}
     if json_file:
         with open(json_file) as f:
