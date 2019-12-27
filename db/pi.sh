bbb77f6b18c5295944da5cbbba55171ac1b9befe
Elie Khoury <elie@el-khoury.net>
2019-12-27 14:04:10 +0100

    dockerfiles now working

---
+++ db/pi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    create database pi;
+    create user sa with encrypted password 'sa';
+    grant all privileges on database pi to sa;
+
+    \c pi
+    
+    CREATE TABLE dust (
+        enddate timestamp without time zone,
+        value double precision,
+        voltage double precision,
+        density double precision,
+        startdate timestamp without time zone,
+        id bigserial PRIMARY KEY
+    );
+
+    CREATE TABLE temperature (
+        temperature double precision,
+        humidity double precision,
+        "timestamp" timestamp without time zone,
+        id bigserial PRIMARY KEY
+    );
+
+    GRANT SELECT,UPDATE,DELETE,INSERT ON ALL TABLES IN SCHEMA public TO sa; 
+EOSQL
