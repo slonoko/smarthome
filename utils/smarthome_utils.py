ac65e2c4d5d33dc70bb56b913928c0c206904c0f
Elie Khoury <elie@el-khoury.net>
2019-12-29 08:40:43 +0100

    removed an unused function

--- utils/smarthome_utils.py
+++ utils/smarthome_utils.py
@@ -3,13 +3,6 @@ import sys
 import datetime
 import os
 
-
-def params():
-    properties = ['CX_SPARK_URL', 'CX_KAFKA_URL', 'CX_DB_URL',
-                  'CX_DB_USER', 'CX_DB_PWD', 'CX_DB_DRIVER', 'CX_DB_PREFIX']
-    return properties
-
-
 def corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
