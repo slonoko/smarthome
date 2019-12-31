9951eeb998a2d89dbf48a3ef4fd2f04debe63c10
Elie Khoury <elie@el-khoury.net>
2019-12-31 08:05:45 +0100

    fixed

--- backless/dust/handler.js
+++ backless/dust/handler.js
@@ -6,12 +6,10 @@ const { Pool, Client } = require('pg');
 module.exports = {
   dust(event, context) {
     const pool = new Pool();
-    var _time;
     pool.query('SELECT NOW()', (err, res) => {
       console.log(err, res);
-      _time = res;
       pool.end();
+      return res.rows[0];
     });
-    return {'now': 'test2'};
-  },
+  }
 };
