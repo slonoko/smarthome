f8a6650522ff66b74d8da655698951689887c223
Elie Khoury <elie@el-khoury.net>
2019-12-31 09:40:52 +0100

    first version of serverless api is ready

---
+++ backless/js/temperature.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const _ = require("lodash");
+const { Pool, Client } = require("pg");
+
+module.exports = {
+  current: (event, context) =>
+    new Promise((resolve, reject) => {
+      const pool = new Pool();
+      pool
+        .query("SELECT NOW()")
+        .then(res => {
+          console.info(`now is ${res.rows[0].now}`);
+          var cur_time = res.rows[0].now;
+          pool.end();
+          resolve({ now: cur_time });
+        })
+        .catch(err => {
+          console.error(err);
+          reject({ error: err.stack });
+        });
+    }),
+  range: (event, context) => new Promise((resolve, reject) =>{
+    resolve({'value':'ok'});
+  })
+};
