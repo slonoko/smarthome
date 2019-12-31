0833533155680f5967d23d07edc50207cc84b20d
Elie Khoury <elie@el-khoury.net>
2019-12-31 14:55:47 +0100

    code

--- backless/js/dust.js
+++ backless/js/dust.js
@@ -8,19 +8,38 @@ module.exports = {
     new Promise((resolve, reject) => {
       const pool = new Pool();
       pool
-        .query("SELECT NOW()")
+        .query("select * from dust order by enddate desc limit 1")
         .then(res => {
-          console.info(`now is ${res.rows[0].now}`);
-          var cur_time = res.rows[0].now;
+          var data = { value: "empty" };
+          if (res.rows.length > 0) data = res.rows[0];
           pool.end();
-          resolve({ now: cur_time });
+          resolve(data);
         })
         .catch(err => {
           console.error(err);
           reject({ error: err.stack });
         });
     }),
-  range: (event, context) => new Promise((resolve, reject) =>{
-    resolve({'value':'ok'});
-  })
+  range: (event, context) =>
+    new Promise((resolve, reject) => {
+      const pool = new Pool();
+
+      const query = {
+        text:
+          "select * from dust where startdate between $1::timestamp and $2::timestamp order by startdate desc",
+        values: [event.data.from_date, event.data.to_date]
+      };
+      pool
+        .query(query)
+        .then(res => {
+          var data = { value: "empty" };
+          if (res.rows.length > 0) data = res.rows;
+          pool.end();
+          resolve(data);
+        })
+        .catch(err => {
+          console.error(err);
+          reject({ error: err.stack });
+        });
+    })
 };
