e432f9ce379b4685675c8fc4977f2b94f724c76c
Elie Khoury <elie@el-khoury.net>
2019-12-31 07:50:23 +0100

    using  nodejs for the serverless backend

---
+++ backless/temperature/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const _ = require('lodash');
+const { Pool, Client } = require('pg');
+
+module.exports = {
+  temperature(event, context) {
+    const pool = new Pool();
+    var _time;
+    pool.query('SELECT NOW()', (err, res) => {
+      console.log(err, res);
+      _time = res;
+      pool.end();
+    });
+    return {'now': 'test2'};
+  },
+};
