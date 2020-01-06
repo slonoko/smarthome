db1467bc68d9762dfb8959107fe432b0f4b00efd
Elie Khoury <elie@el-khoury.net>
2020-01-06 23:19:54 +0100

    better styling

--- frontend/src/component/InfoCard.jsx
+++ frontend/src/component/InfoCard.jsx
@@ -5,15 +5,14 @@ import './InfoCard.scss';
 class InfoCard extends Component {
   render() {
     return (
-      <Card bg="info" text="white" style={{ minWidth: '18rem' }}>
-        <Card.Header>Header</Card.Header>
+      <Card bg="success" text="white" style={{ minWidth: '18rem' }}>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
           </Card.Text>
         </Card.Body>
+        <Card.Footer className="light">2 days ago</Card.Footer>
       </Card>
     );
   }
