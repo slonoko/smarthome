4ba4e8f0e974eb09231b976127886451096bcbc5
Elie Khoury <elie@el-khoury.net>
2020-01-07 22:41:48 +0100

    polling data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,38 @@ import InfoCard from './components/infocard/infocard.component';
 import InfoDetails from './components/infodetails/infodetails.component';
 import { Route, Switch } from "react-router-dom";
 
-class App extends React.Component {
+interface IState {
+  temperature: any,
+  dust:any
+}
+
+interface IProps {
+}
+
+class App extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      temperature: {},
+      dust: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_API_HOSTNAME+'/temperature/')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ temperature: data })
+    })
+    .catch(console.error)
+    fetch(process.env.REACT_APP_API_HOSTNAME+'/dust/')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ dust: data })
+    })
+    .catch(console.error)
+  }
 
   render() {
     return (
@@ -13,15 +44,15 @@ class App extends React.Component {
         <CardDeck>
           <InfoCard color="success" icon="thermometer-half" path="/temperature">
             <div className="float-right">
-              <div className="float-left big-text">25</div><div className="float-right small-text">°C</div>
+    <div className="float-left big-text">{(this.state.temperature.temperature == undefined)?"-":this.state.temperature.temperature }</div><div className="float-right small-text">°C</div>
             </div>
             <div>
-              <div className="float-left big-text">45</div><div className="float-right small-text">g/m<sup>3</sup></div>
+              <div className="float-left big-text">{(this.state.temperature.humidity == undefined)?"-":this.state.temperature.humidity }</div><div className="float-right small-text">g/m<sup>3</sup></div>
             </div>
           </InfoCard>
           <InfoCard color="info" icon="wind" path="/dust">
             <div className="float-right">
-              <div className="float-left big-text">25</div><div className="float-right small-text">µg/m<sup>3</sup></div><br />
+              <div className="float-left big-text">{(this.state.dust.density == undefined)?"-":(Math.round(this.state.dust.density * 100) / 100) }</div><div className="float-right small-text">µg/m<sup>3</sup></div><br />
             </div>
           </InfoCard>
           <InfoCard color="warning" icon="bullhorn" path="/noise">
