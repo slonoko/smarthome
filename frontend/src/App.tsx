a5f05ec83e5e32cff6153babd0175d53dc1aa9d5
ELIE KHOURY <elie.khoury@daimler.com>
2020-01-08 08:50:54 +0000

    making a poller

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ interface IProps {
 }
 
 class App extends React.Component<IProps, IState> {
+  poller:any;
 
   constructor(props: IProps) {
     super(props);
@@ -24,18 +25,29 @@ class App extends React.Component<IProps, IState> {
   }
 
   componentDidMount() {
+    this.poller = setTimeout(this.pullData, 60000);
+    this.pullData();
+  }
+
+  pullData() {
     fetch(process.env.REACT_APP_API_HOSTNAME + '/temperature/')
       .then(res => res.json())
       .then((data) => {
-        this.setState({ temperature: data })
+        this.setState({ temperature: data });
       })
-      .catch(console.error)
+      .catch(err =>{
+        clearTimeout(this.poller);
+        console.error("Error occured, stopping timer!\n"+err.message);
+      })
     fetch(process.env.REACT_APP_API_HOSTNAME + '/dust/')
       .then(res => res.json())
       .then((data) => {
-        this.setState({ dust: data })
+        this.setState({ dust: data });
       })
-      .catch(console.error)
+      .catch(err => {
+        clearTimeout(this.poller);
+        console.error("Error occured, stopping timer!\n"+err.message);
+      });
   }
 
   render() {
@@ -44,15 +56,15 @@ class App extends React.Component<IProps, IState> {
         <CardDeck>
           <InfoCard color="success" icon="thermometer-half" path="/temperature">
             <div className="float-right">
-              <div className="float-left big-text">{(this.state.temperature.temperature == undefined) ? "-" : this.state.temperature.temperature}</div><div className="float-right small-text">°C</div>
+              <div className="float-left big-text">{(this.state.temperature.temperature === undefined) ? "-" : this.state.temperature.temperature}</div><div className="float-right small-text">°C</div>
             </div>
             <div>
-              <div className="float-left big-text">{(this.state.temperature.humidity == undefined) ? "-" : this.state.temperature.humidity}</div><div className="float-right small-text">g/m<sup>3</sup></div>
+              <div className="float-left big-text">{(this.state.temperature.humidity === undefined) ? "-" : this.state.temperature.humidity}</div><div className="float-right small-text">g/m<sup>3</sup></div>
             </div>
           </InfoCard>
           <InfoCard color="info" icon="wind" path="/dust">
             <div className="float-right">
-              <div className="float-left big-text">{(this.state.dust.density == undefined) ? "-" : (Math.round(this.state.dust.density * 100) / 100)}</div><div className="float-right small-text">µg/m<sup>3</sup></div><br />
+              <div className="float-left big-text">{(this.state.dust.density === undefined) ? "-" : (Math.round(this.state.dust.density * 100) / 100)}</div><div className="float-right small-text">µg/m<sup>3</sup></div><br />
             </div>
           </InfoCard>
           <InfoCard color="warning" icon="bullhorn" path="/noise">
