9637e66549eddddf216cc51a840e4347bfe65b1a
Elie Khoury <elie.kh@gmail.com>
2020-01-10 12:24:27 +0100

    db repo renamed

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 eval $(minikube docker-env)
 
-docker build backend -t localhost:32000/smarthome-api:0.8
 docker build db -t localhost:32000/smarthome-db:0.8
 docker build frontend -t localhost:32000/smarthome-webui:0.8
 docker build spark -t localhost:32000/smarthome-spark:0.8
+docker build backend -t localhost:32000/smarthome-api:0.8
 
 # docker save smarthome-api > tars/smarthome-api.tar
 # docker save smarthome-db > tars/smarthome-db.tar
@@ -17,7 +17,7 @@ docker tag smarthome-db:0.8 localhost:32000/smarthome-db:0.8
 docker tag smarthome-webui:0.8 localhost:32000/smarthome-webui:0.8
 docker tag smarthome-spark:0.8 localhost:32000/smarthome-spark:0.8
 
-docker push localhost:32000/smarthome-api:0.8
 docker push localhost:32000/smarthome-db:0.8
+docker push localhost:32000/smarthome-api:0.8
 docker push http://localhost:32000/smarthome-webui:0.8
 docker push localhost:32000/smarthome-spark:0.8
