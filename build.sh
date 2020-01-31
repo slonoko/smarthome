c8285db1e5cdc9761aec61b0bac05c44b93a7c64
Elie Khoury <elie.kh@gmail.com>
2020-01-31 20:20:58 +0100

    minor tweaks

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-eval $(minikube docker-env)
 
 docker build backend -t smarthome-api:0.8
 docker build db -t smarthome-db:0.8
 docker build frontend -t smarthome-webui:0.8
 docker build spark -t smarthome-spark:0.8
 
-docker tag smarthome-api:0.8 cx:5000/smarthome-api:0.8
-docker tag smarthome-db:0.8 cx:5000/smarthome-db:0.8
-docker tag smarthome-webui:0.8 cx:5000/smarthome-webui:0.8
-docker tag smarthome-spark:0.8 cx:5000/smarthome-spark:0.8
+docker tag smarthome-api:0.8 localhost:5000/smarthome-api:0.8
+docker tag smarthome-db:0.8 localhost:5000/smarthome-db:0.8
+docker tag smarthome-webui:0.8 localhost:5000/smarthome-webui:0.8
+docker tag smarthome-spark:0.8 localhost:5000/smarthome-spark:0.8
 
-docker push cx:5000/smarthome-api:0.8
-docker push cx:5000/smarthome-db:0.8
-docker push cx:5000/smarthome-webui:0.8
-docker push cx:5000/smarthome-spark:0.8
+docker push localhost:5000/smarthome-api:0.8
+docker push localhost:5000/smarthome-db:0.8
+docker push localhost:5000/smarthome-webui:0.8
+docker push localhost:5000/smarthome-spark:0.8
