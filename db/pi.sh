2ea70c15c93794ed73466d3ea4b168477c81ea2c
Elie Khoury <elie@el-khoury.net>
2020-02-02 09:12:02 +0100

    storing  private info in secrets

--- db/pi.sh
+++ db/pi.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# docker run --rm -it -p 5432:5432/tcp  -e POSTGRES_PASSWORD=postgres -v "/home/elie/Workspace/smarthome/db/":"/docker-entrypoint-initdb.d/" postgres:12.0-alpine
+
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    create database pi;
-    create user sa with encrypted password 'sa';
-    grant all privileges on database pi to sa;
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+    create database $CX_DB_NAME;
+    create user $CX_DB_USER with encrypted password '$CX_DB_PWD';
+    grant all privileges on database $CX_DB_NAME to $CX_DB_USER;
 
-    \c pi
+    \c $CX_DB_NAME
     
     CREATE TABLE dust (
         enddate timestamp without time zone,
@@ -24,7 +26,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         id bigserial PRIMARY KEY
     );
 
-    GRANT SELECT,UPDATE,DELETE,INSERT ON ALL TABLES IN SCHEMA public TO sa; 
-    grant all on sequence temperature_id_seq to sa;
-    grant all on sequence dust_id_seq to sa;
+    GRANT SELECT,UPDATE,DELETE,INSERT ON ALL TABLES IN SCHEMA public TO $CX_DB_USER; 
+    grant all on sequence temperature_id_seq to $CX_DB_USER;
+    grant all on sequence dust_id_seq to $CX_DB_USER;
 EOSQL
